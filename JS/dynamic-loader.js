b3e0847893c422aae76503145c562bb73264b6ca
SwiftMaster <147448007+SwiftMaster-cmd@users.noreply.github.com>
2025-06-04 11:22:26 -0500

    few

--- JS/dynamic-loader.js
+++ JS/dynamic-loader.js
@@ -7,17 +7,16 @@ import {
 
 document.addEventListener('DOMContentLoaded', () => {
   // 🔧 Firebase config
-  const firebaseConfig = {
-    apiKey: "AIzaSyCf_se10RUg8i_u8pdowHlQvrFViJ4jh_Q",
-    authDomain: "mishanikeyenko.firebaseapp.com",
-    databaseURL: "https://mishanikeyenko-default-rtdb.firebaseio.com",
-    projectId: "mishanikeyenko",
-    storageBucket: "mishanikeyenko.appspot.com",
-    messagingSenderId: "1089190937368",
-    appId: "1:1089190937368:web:959c825fc596a5e3ae946d",
-    measurementId: "G-L6CC27129C"
-  };
-
+const firebaseConfig = {
+  apiKey: "AIzaSyCf_se10RUg8i_u8pdowHlQvrFViJ4jh_Q",
+  authDomain: "mishanikeyenko.firebaseapp.com",
+  databaseURL: "https://mishanikeyenko-default-rtdb.firebaseio.com",
+  projectId: "mishanikeyenko",
+  storageBucket: "mishanikeyenko.firebasestorage.app",
+  messagingSenderId: "1089190937368",
+  appId: "1:1089190937368:web:959c825fc596a5e3ae946d",
+  measurementId: "G-L6CC27129C"
+};
   const app = initializeApp(firebaseConfig);
   const db = getDatabase(app);
   const scriptsRef = ref(db, 'jsModules');
