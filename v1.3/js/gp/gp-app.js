a01d5c634664998a68e49b7ff5cb15a16de6191d
SwiftMaster <147448007+SwiftMaster-cmd@users.noreply.github.com>
2025-08-01 07:18:37 -0500

    fes

--- v1.3/js/gp/gp-app.js
+++ v1.3/js/gp/gp-app.js
@@ -181,7 +181,7 @@ function setProgress(pct) {
 
 // Back to dashboard
 dashboardBtn.onclick = () => {
-  window.location.href = "../v1/html/admin.html";
+  window.location.href = "../html/admin.html";
 };
 
 // New lead (calls your app logic)
